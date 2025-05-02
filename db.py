8032fb2a3ff31b2dbc643718cb885ca8b05d656c
Kevin Nguyen <kevin.m.nguyen@colorado.edu>
2025-05-02 16:31:05 -0600

    Update db.py

--- db.py
+++ db.py
@@ -1,6 +1,6 @@
 import pandas as pd
 def fetch_sic_data():
-    df = pd.read_excel("sic_codes.xlsx")
+    df = pd.read_excel("sic_codes.xlsx", dtype=str)
     df['sic8'] = df['sic8'].astype(str).str.strip()
     df['Description'] = df['Description'].str.strip()
     return df
